3545e3bce34ed0ff03645f5879661bd66296743b
wangfeng <wangfeng@iri.cn>
2018-11-16 15:40:43 +0800

    4.0 commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import WidgetBase from "@dojo/framework/widget-core/WidgetBase";
+import renderer from '@dojo/framework/widget-core/vdom';
 import Hello from "./widgets/Hello";
 import { v, w } from "@dojo/framework/widget-core/d";
-import { ProjectorMixin } from "@dojo/framework/widget-core/mixins/Projector";
 import { Registry } from "@dojo/framework/widget-core/Registry";
 import { registerThemeInjector } from "@dojo/framework/widget-core/mixins/Themed";
 import designerTheme from "./themes/default/theme";
 import TabController from "@dojo/widgets/tab-controller/index";
 import Tab from "@dojo/widgets/tab/index";
+import '@dojo/themes/dojo/index.css'
 // import { registerThemeInjector } from "@dojo/framework/widget-core/mixins/Themed";
 // import designerTheme from "./themes/default/theme";
 
@@ -32,10 +33,5 @@ class App extends WidgetBase {
 
 const registry = new Registry();
 registerThemeInjector(designerTheme, registry);
-const Projector = ProjectorMixin(App);
-const projector = new Projector();
-projector.setProperties({ registry });
-
-// By default, append() will attach the rendered content to document.body.  To insert this application
-// into existing HTML content, pass the desired root node to append().
-projector.append();
+const r = renderer(() => w(App, {}));
+r.mount({ registry });
